bc283bb35b6d3ce8736dff8cfd8be492f56bd6cd
Jayadev1127 <jayadev1127@gmail.com>
2020-12-11 17:12:19 -0700

    PatientAgent,Konsole

---
+++ SENG696-Project/src/meeting/scheduler/Settings.java
@@ -0,0 +1,8 @@
+package meeting.scheduler;
+
+
+public class Settings {
+    public static final boolean debug = true;
+    public static final int numDays = 5;
+    public static final int numTimeSlots = 4;
+}
